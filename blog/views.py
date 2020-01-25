b174d0150826962392df344df4909152d78e359a
Smit Patel <smitpat2000@gmail.com>
2020-01-25 11:36:00 +0530

    Modified template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {'name': 'Smit'})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by("author")
+    return render(request, 'blog/post_list.html', {'name': 'Smit', 'posts': posts})
